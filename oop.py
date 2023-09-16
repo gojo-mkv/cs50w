ce274a2ecf9039e5b79daaaf85734b2ffb24f2d0
akshay <akshayrajanpc@outlook.com>
2023-09-16 17:43:10 +0530

    Add --init

---
+++ oop.py
@@ -0,0 +1,7 @@
+class Point():
+    # Function that is automatically called every time we try to create 'Point'
+    def __init__(self, x, y):    # All functions acting on object itself are called Methods
+        # Allow data to be stored inside the object
+        self.x = x
+        self.y = y
+        
