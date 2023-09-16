ef0c13fd10051ac4a986b25432f2d3f032be127c
akshay <akshayrajanpc@outlook.com>
2023-09-16 18:10:00 +0530

    class Flight

--- oop.py
+++ oop.py
@@ -8,3 +8,27 @@ class Point():
 p = Point(2, 8)
 print(p.x)
 print(p.y)
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers. append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["harry", "ron", "hermione", "ginny"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight")
+    else:
+        print(f"No available seats for {person}")
